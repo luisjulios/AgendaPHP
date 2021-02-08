5ba3ff6cb0f79ae635b3b49f30bdf97af3487690
luisjulios <antonio.saurith@gmail.com>
2021-02-08 14:52:37 -0300

    Iniciando los archivos bases

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php	include 'inc/layout/header.php'?>
+<div class="contenedor-barra">
+<h1>Agenda de Contactos</h1>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php	include 'inc/layout/footer.php'?>
