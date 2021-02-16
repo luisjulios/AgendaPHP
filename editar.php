93a9b219d627c5010354b7b7a2a58db5f3d284b1
luisjulios <antonio.saurith@gmail.com>
2021-02-16 11:48:47 -0300

    Actualizando CSS y JS

---
+++ editar.php
@@ -0,0 +1,15 @@
+<?php	include 'inc/layout/header.php'?>
+  <div class="contenedor-barra">
+    <div class="contenedor barra">
+      <a href="index.php" class="btn volver">Volver</a>
+      <h1>Editar contacto</h1>
+    </div>
+  </div>
+  <div class="bg-secundario contenedor sombra">
+    <form action="#" id="contacto">
+    <legend>Edite el contacto</legend>
+    <?php	include 'inc/layout/formulario.php'?>
+  </form>
+  </div>
+
+<?php	include 'inc/layout/footer.php'?>
--- index.php
+++ index.php
@@ -1,39 +1,76 @@
 <?php	include 'inc/layout/header.php'?>
-<div class="contenedor-barra">
-<h1>Agenda de Cont</h1>
-</div>
+  <div class="contenedor-barra">
+    <h1>Agenda de Contacto</h1>
+  </div>
+
+  <div class="bg-secundario contenedor sombra">
+    <form action="#" id="contacto">
+    <legend>Añada un contacto <span>Todos los campos son obligatorios</span></legend>
+
+    <?php	include 'inc/layout/formulario.php'?>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    </form>
+  </div>
+  <div class="bg-blanco contenedor sombra contactos">
+    <div class="contenedor-contactos">
+      <h2>Contactos</h2>
+        <input type="text" id="buscar" class="buscador sombra" placeholder="Buscar contactos...">
+        <p class="total-contactos"><span>2</span> Contactos</p>
+        <div class="contenedor-tabla">
+          <table id="listado-contactos" class="listado-contactos">
+            <thead>
+              <tr>
+                <th>Nombre</th>
+                <th>Empresa</th>
+                <th>Teléfono</th>
+                <th>Acciones</th>
+              </tr>
+              <tbody>
+                <tr>
+                  <td>Luis</td>
+                  <td>Umbrella</td>
+                  <td>996536109</td>
+                  <td>
+                    <a class="btn-editar btn" href="editar.php?id=1">
+                      <i class="far fa-edit"></i>
+                    </a>
+                    <button data-id="1" type="button" class="btn-borrar btn">
+                      <i class="far fa-trash-alt"></i>
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Luis</td>
+                  <td>Umbrella</td>
+                  <td>996536109</td>
+                  <td>
+                    <a class="btn-editar btn" href="editar.php?id=1">
+                      <i class="far fa-edit"></i>
+                    </a>
+                    <button data-id="1" type="button" class="btn-borrar btn">
+                      <i class="far fa-trash-alt"></i>
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Luis</td>
+                  <td>Umbrella</td>
+                  <td>996536109</td>
+                  <td>
+                    <a class="btn-editar btn" href="editar.php?id=1">
+                      <i class="far fa-edit"></i>
+                    </a>
+                    <button data-id="1" type="button" class="btn-borrar btn">
+                      <i class="far fa-trash-alt"></i>
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </thead>
+          </table>
+        </div>
+    </div>
+  </div>
 
 <?php	include 'inc/layout/footer.php'?>
